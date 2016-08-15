6e49f6edc6865ebfd08a30537061e92bfbd3e32c
TomIrimescu <tom@irimescu.com>
2016-08-15 14:16:47 -0400

    Commented out code for lifecycle exercise to display main exercises
    On branch master
    Changes to be committed:
    modified:   src/app/app.component.js
    modified:   src/app/app.component.js.map
    modified:   src/app/app.component.ts

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,16 +8,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        /* this commented code is for the Component Lifecycle Usage Examples*/
-        this.delete = false;
-        this.test = 'Starting value';
-        this.boundValue = 1000;
     }
     AppComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'app-root',
-            template: "\n<!--\t\t<h1>Root Component</h1>\n\t\t<h3> Template HTML</h3>\n\t\t<app-databinding></app-databinding>-->\n<!-- this commented code is for the Component Lifecycle Usage Examples-->\n\t\t<app-lifecycle *ngIf=\"!delete\" [bindable]=\"boundValue\">\n\t\t\t<p #boundContent>{{test}}</p>\n\t\t</app-lifecycle>\n\t\t<button (click)=\"test = 'Changed value'\">Click to Change Content</button>\n\t\t<button (click)=\"boundValue = 2000\">Click to Change Binding</button>\n\t\t<button (click)=\"delete = true\">Click to Delete</button>\n\t"
+            template: "\n\t\t<h1>Root Component</h1>\n\t\t<h3> Template HTML</h3>\n\t\t<app-databinding></app-databinding>\n<!-- this commented code is for the Component Lifecycle Usage Examples-->\n\t\t<!--<app-lifecycle *ngIf=\"!delete\" [bindable]=\"boundValue\">-->\n\t\t\t<!--<p #boundContent>{{test}}</p>-->\n\t\t<!--</app-lifecycle>-->\n\t\t<!--<button (click)=\"test = 'Changed value'\">Click to Change Content</button>-->\n\t\t<!--<button (click)=\"boundValue = 2000\">Click to Change Binding</button>-->\n\t\t<!--<button (click)=\"delete = true\">Click to Delete</button>-->\n\t"
         })
     ], AppComponent);
     return AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,21 @@ import { Component } from '@angular/core';
 	moduleId: module.id,
 	selector: 'app-root',
 	template: `
-<!--		<h1>Root Component</h1>
+		<h1>Root Component</h1>
 		<h3> Template HTML</h3>
-		<app-databinding></app-databinding>-->
+		<app-databinding></app-databinding>
 <!-- this commented code is for the Component Lifecycle Usage Examples-->
-		<app-lifecycle *ngIf="!delete" [bindable]="boundValue">
-			<p #boundContent>{{test}}</p>
-		</app-lifecycle>
-		<button (click)="test = 'Changed value'">Click to Change Content</button>
-		<button (click)="boundValue = 2000">Click to Change Binding</button>
-		<button (click)="delete = true">Click to Delete</button>
+		<!--<app-lifecycle *ngIf="!delete" [bindable]="boundValue">-->
+			<!--<p #boundContent>{{test}}</p>-->
+		<!--</app-lifecycle>-->
+		<!--<button (click)="test = 'Changed value'">Click to Change Content</button>-->
+		<!--<button (click)="boundValue = 2000">Click to Change Binding</button>-->
+		<!--<button (click)="delete = true">Click to Delete</button>-->
 	`
 })
 export class AppComponent {
 /* this commented code is for the Component Lifecycle Usage Examples*/
-	delete = false;
+/*	delete = false;
 	test = 'Starting value';
-	boundValue = 1000;
+	boundValue = 1000;*/
 }
